3a9a1f07066d56f40b69d4e0064ed2506b51a48b
defaultuser0 <axolotlslmao@gmail.com>
2024-12-29 17:26:21 -0600

    ifhsaduuojjkn

---
+++ CapuchinTemplate/Mod.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime;
+using Photon;
+using MelonLoader;
+using CapuchinTemplate;
+using UnityEngine;
+
+[assembly: MelonInfo(typeof(ModCodeThingieYea), ModInfo.Name, ModInfo.Description, ModInfo.Version, ModInfo.Author)]
+[assembly: MelonGame("Duttbust", "Capuchin")]
+
+namespace CapuchinTemplate
+{
+    public class ModCodeThingieYea : MelonMod
+    {
+        public override void OnSceneWasLoaded(int buildindex, string sceneName) // Runs when a Scene has Loaded and is passed the Scene's Build Index and Name.
+        {
+            if (sceneName == "CapuchinCopy")
+            {
+                GameObject.Find("Global/Levels/Forest/Props/Speaker/").SetActive(false);
+                GameObject.Find("Global/Levels/Forest/Props/Chill Cabin/BluetoothSpeakerParent/BluetoothSpeaker/").SetActive(false);
+                GameObject.Find("Global/Levels/Capuchin_Pier/Speaker (1)/ForestMusic").SetActive(false);
+            }
+        }
+    }
+}
---
+++ CapuchinTemplate/ModInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CapuchinTemplate
+{
+    public class ModInfo
+    {
+        public const string Name = "nomusic";
+        public const string Description = "no music";
+        public const string Author = "defaultuser0";
+        public const string Version = "1.0.0";
+    }
+}
